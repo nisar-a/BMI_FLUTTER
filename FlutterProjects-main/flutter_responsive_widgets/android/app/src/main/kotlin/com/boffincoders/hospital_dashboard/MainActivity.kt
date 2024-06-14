bddc75c3af8dcdceadfec9657134be4d258f922b
Nisar.A <124667527+nisar-a@users.noreply.github.com>
2024-06-14 21:57:54 +0530

    Delete FlutterProjects-main/flutter_responsive_widgets directory

--- FlutterProjects-main/flutter_responsive_widgets/android/app/src/main/kotlin/com/boffincoders/hospital_dashboard/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.boffincoders.hospital_dashboard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- FlutterProjects-main/flutter_responsive_widgets/android/app/src/main/kotlin/com/example/flutter_responsive_widgets/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_responsive_widgets
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
